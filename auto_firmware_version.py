5c6810dff47a3547dd2dcf8c3e4b3d22618c8a11
Jochen Cabrera <jochen.matz@googlemail.com>
2022-09-24 00:15:33 +0200

    create BIN File on Project Information

--- auto_firmware_version.py
+++ auto_firmware_version.py
@@ -1,14 +1,22 @@
 import subprocess
+import os
 Import("env")
 
 def get_firmware_specifier_build_flag():    
-    ret = subprocess.run(["git", "describe"], stdout=subprocess.PIPE, text=True) #Uses only annotated tags
-    #ret = subprocess.run(["git", "describe", "--tags"], stdout=subprocess.PIPE, text=True) #Uses any tags
-    build_version = ret.stdout.strip()
-    build_flag = "-D AUTO_VERSION=\\\"" + build_version + "\\\""
-    print ("Firmware Revision: " + build_version)
-    return (build_flag)
+  ret = subprocess.run(["git", "describe"], stdout=subprocess.PIPE, text=True) #Uses only annotated tags
+  build_version = ret.stdout.strip()
+  build_flag = "-D AUTO_VERSION=\\\"" + build_version + "\\\""
+  return (build_flag)
+
+def get_program_name():
+  my_flags = env.ParseFlags(env['BUILD_FLAGS'])
+  defines = {k: v for (k, v) in my_flags.get("CPPDEFINES")}
+  return ("FW_" + os.path.basename(os.getcwd()) + "_" + defines.get("AUTO_VERSION"))
 
 env.Append(
-    BUILD_FLAGS=[get_firmware_specifier_build_flag()]
+  BUILD_FLAGS=[get_firmware_specifier_build_flag()]
+)
+
+env.Replace(
+  PROGNAME=get_program_name()
 )
