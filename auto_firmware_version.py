fb6267985b830c60fd152442f1805d193cfe0b19
Jochen Cabrera <jochen.matz@googlemail.com>
2023-03-09 17:14:42 +0100

    Script Firmeware version

--- auto_firmware_version.py
+++ auto_firmware_version.py
@@ -1,11 +1,9 @@
 import subprocess
 import os
-#Import("env")
-
-print("Hello World")
+Import("env")
 
 def get_firmware_specifier_build_flag():    
-  ret = subprocess.run(["git", "describe"], stdout=subprocess.PIPE, text=True) #Uses only annotated tags
+  ret = subprocess.run(["git", "describe", "--tags"], stdout=subprocess.PIPE, text=True) #Uses only annotated tags
   build_version = ret.stdout.strip()
   build_flag = "-D AUTO_VERSION=\\\"" + build_version + "\\\""
   return (build_flag)
@@ -13,14 +11,12 @@ def get_firmware_specifier_build_flag():
 def get_program_name():
   my_flags = env.ParseFlags(env['BUILD_FLAGS'])
   defines = {k: v for (k, v) in my_flags.get("CPPDEFINES")}
-  return ("FW_" + os.path.basename(os.getcwd()) + "_")# + "_" + defines.get("AUTO_VERSION"))
+  return ("FW_" + os.path.basename(os.getcwd()) + "_" + defines.get("AUTO_VERSION"))
 
-get_firmware_specifier_build_flag()
+env.Append(
+  BUILD_FLAGS=[get_firmware_specifier_build_flag()]
+)
 
-#env.Append(
-#  BUILD_FLAGS=[get_firmware_specifier_build_flag()]
-#)
-#
-#env.Replace(
-#  PROGNAME=get_program_name()
-#)
+env.Replace(
+  PROGNAME=get_program_name()
+)
